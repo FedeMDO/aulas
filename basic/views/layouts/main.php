08e87e041a1f7a4377c38a10600ec55291384b50
Cristian Pinto <wikendar@gmail.com>
2018-05-03 15:46:59 -0300

    nuevo boton sedes navbar

--- basic/views/layouts/main.php
+++ basic/views/layouts/main.php
@@ -41,7 +41,7 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
+            ['label' => 'Sedes', 'url' => ['/sede/vistav']],
             ['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
                 ['label' => 'Login', 'url' => ['/site/login']]
