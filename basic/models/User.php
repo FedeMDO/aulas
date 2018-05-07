c668a7ddc0cf75e38e3df84fd98e47259f5c20af
Gabit <gabri.bentez@gmail.com>
2018-05-07 13:13:56 -0300

    modifico models/user.php para porder loguearse, hay un problema con la confirmacion del registro del usuario no activa el usuario y no se puede loguear a menos que se modifique el estado desde la base de datos

--- basic/models/User.php
+++ basic/models/User.php
@@ -1,48 +1,52 @@
 <?php
 
 namespace app\models;
+use yii\web\IdentityInterface;
+
 
 class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
 {
+    
     public $id;
     public $username;
+    public $email;
     public $password;
     public $authKey;
     public $accessToken;
-
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
-
+    public $activate;
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
+    
+    /* busca la identidad del usuario a través de su $id */
+
     public static function findIdentity($id)
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        
+        $user = Users::find()
+                ->where("activate=:activate", [":activate" => 1])
+                ->andWhere("id=:id", ["id" => $id])
+                ->one();
+        
+        return isset($user) ? new static($user) : null;
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
+    
+    /* Busca la identidad del usuario a través de su token de acceso */
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        foreach (self::$users as $user) {
-            if ($user['accessToken'] === $token) {
+        
+        $users = Users::find()
+                ->where("activate=:activate", [":activate" => 1])
+                ->andWhere("accessToken=:accessToken", [":accessToken" => $token])
+                ->all();
+        
+        foreach ($users as $user) {
+            if ($user->accessToken === $token) {
                 return new static($user);
             }
         }
@@ -51,15 +55,25 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
     }
 
     /**
+     * 
+     * 
+     * 
      * Finds user by username
      *
-     * @param string $username
+     * @param  string      $username
      * @return static|null
      */
+    
+    /* Busca la identidad del usuario a través del username */
     public static function findByUsername($username)
     {
-        foreach (self::$users as $user) {
-            if (strcasecmp($user['username'], $username) === 0) {
+        $users = Users::find()
+                ->where("activate=:activate", ["activate" => 1])
+                ->andWhere("username=:username", [":username" => $username])
+                ->all();
+        
+        foreach ($users as $user) {
+            if (strcasecmp($user->username, $username) === 0) {
                 return new static($user);
             }
         }
@@ -68,24 +82,30 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
+    
+    /* Regresa el id del usuario */
     public function getId()
     {
         return $this->id;
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
+    
+    /* Regresa la clave de autenticación */
     public function getAuthKey()
     {
         return $this->authKey;
     }
 
     /**
-     * {@inheritdoc}
+     * @inheritdoc
      */
+    
+    /* Valida la clave de autenticación */
     public function validateAuthKey($authKey)
     {
         return $this->authKey === $authKey;
@@ -94,11 +114,15 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
     /**
      * Validates password
      *
-     * @param string $password password to validate
-     * @return bool if password provided is valid for current user
+     * @param  string  $password password to validate
+     * @return boolean if password provided is valid for current user
      */
     public function validatePassword($password)
     {
-        return $this->password === $password;
+        /* Valida el password */
+        if (crypt($password, $this->password) == $this->password)
+        {
+        return $password === $password;
+        }
     }
 }
