99e3bebfdfd57704a6ff33301de526866b6c5f12
Cristian Pinto <wikendar@gmail.com>
2018-05-13 21:52:50 -0300

    nuevas vista css

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -21,7 +21,7 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-    <h1 style="font-size:8vw;">Aulas Unaj</h1>
+    <h1 style="font-size:8vw;">Aulas Unaj V1</h1>
 <p style="font-size:2vw;">Sistema de Gestion de Aulas</p>
         
     </div>
