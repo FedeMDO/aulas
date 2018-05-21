ae913d085334a4049ccd40510797e68d4b8a247c
Cristian Pinto <wikendar@gmail.com>
2018-05-21 19:22:24 -0300

    fix login button

--- basic/views/site/index.php
+++ basic/views/site/index.php
@@ -14,28 +14,37 @@ $this->title = 'Universidad Nacional Arturo Jauretche';
 
         
     </div>
-
+            
 
 
 
 <div class="col-md-offset-5 col-md-3">
             <div class="form-login">
             <h4>Bienvenido</h4>
-            <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="username" />
+            <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="Usuario" />
             </br>
-            <input type="text" id="userPassword" class="form-control input-sm chat-input" placeholder="password" />
+            <input type="text" id="userPassword" class="form-control input-sm chat-input" placeholder="Contraseña" />
             </br>
             <div class="wrapper">
             <span class="group-btn">     
-                <a href="#" class="btn btn-success btn-md">login <i class="fa fa-sign-in"></i></a>
+                <a href="#" class="btn btn-success btn-md">Ingresar<i class="fa fa-sign-in"></i></a>
+                <button type="button" class="btn btn-warning">Registrarse</button>
             </span>
+
+    
+
             </div>
             </div>
         
-        </div>
+    </div>
+
+      
 </div>
 
 
-        
+
+
+
+
 
 
