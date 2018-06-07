d668071c3a6b9339f7e7d0565e5eccc8640d0005
FedeMDO <fedee.piola@gmail.com>
2018-06-06 21:36:10 -0300

    Arreglado login modificado models/User.php

--- basic/models/User.php
+++ basic/models/User.php
@@ -14,6 +14,8 @@ class User extends \yii\base\baseObject implements \yii\web\IdentityInterface
     public $authKey;
     public $accessToken;
     public $activate;
+    public $idInstituto;
+    public $rol;
 
     /**
      * @inheritdoc
