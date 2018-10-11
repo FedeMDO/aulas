1c5f3e9f9003f8fd7b93e3edde341c40f0625b0d
Lucas <lucasarielgranata@gmail.com>
2018-10-10 23:55:23 -0300

    funciona modal restri calendar

--- basic/web/js/main2.js
+++ basic/web/js/main2.js
@@ -1,13 +1,33 @@
+ /*$(function(){
+     $(document).on('click', function(){
+         var date = $(this).attr('data-date');
+         $.get('jajaj',{'date':date}, function(data){
+             $('.modal').modal('show')
+             .find('#modelContent')
+             .html(data);
+         });
+     });
+     $('#modelButton').click(function(){
+         $('.modal').modal('show')
+             .find('#modelContent')
+             .load($(this).attr('value'));
+     });
+        
+});*/
 $(function(){
+
     $(document).on('click','.fc-day', function(){
         var date = $(this).attr('data-date');
-        $.get('/restriccion/create',{'date':date}, function(data){
+        
+      var date2 =$('em').text();
+        $.get('/restri/create',{'date':date,'date2':date2}, function(data){
             $('.modal').modal('show')
             .find('#modelContent')
             .html(data);
         });
     });
     $('#modelButton').click(function(){
+       
         $('.modal').modal('show')
             .find('#modelContent')
             .load($(this).attr('value'));
