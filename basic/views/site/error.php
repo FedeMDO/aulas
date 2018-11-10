13ee19f6c6d9d8ff05905a6ba23d68ec616ec86e
magikerN <braianpezet@gmail.com>
2018-11-10 17:28:09 -0300

    Errores personalizados

--- basic/views/site/error.php
+++ basic/views/site/error.php
@@ -9,19 +9,25 @@ use yii\helpers\Html;
 
 $this->title = $name;
 ?>
+
 <div class="site-error">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <div class="alert alert-danger">
-        <?= nl2br(Html::encode($message)) ?>
+<?php if($exception->statusCode==404) : ?>
+    <div class="error">
+    <div class="imagenrobot">
+    <img style="width:500px; margin-left:100px;"src="../image/broken-robot-2.png"/><br><br><h3>OOOPS! no pudimos encontrar esta pagina:</h3><h1 style="font-size:100px">ERROR 404</h1>
     </div>
+    </div>
+<?php elseif($exception->statusCode==403) : ?>
+<div class="error">
+    <div class="imagenrobot">
+    <img style="width:500px;"src="../image/candado.png"/><br><br><h3>Lo sentimos no tienes permisos para realizar esta accion</h3><h1 style="font-size:100px">ERROR 403</h1>
+    </div>
+    </div>
+<?php else : ?>
+<div class="error">
+    <div class="imagenrobot">
+    <img style="width:500px; margin-left:100px;"src="../image/broken-robot-2.png"/><br><br><h3>Lo sentimos ha ocurrido un error:</h3><h1 style="font-size:100px">ERROR<?php echo $exception->statusCode;?></h1>
+    </div>
+    </div>
+<?php endif; ?>
 
-    <p>
-        The above error occurred while the Web server was processing your request.
-    </p>
-    <p>
-        Please contact us if you think this is a server error. Thank you.
-    </p>
-
-</div>
