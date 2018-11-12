d6a42a82812192c28f6ba528381b059b5f018dbe
Lucas <lucasarielgranata@gmail.com>
2018-11-12 17:35:29 -0300

    cambios en form de notificacion

--- basic/views/Admin/_form.php
+++ basic/views/Admin/_form.php
@@ -20,18 +20,20 @@ $form = ActiveForm::begin(
 	]
 );
 ?>
-    <?php $result = ArrayHelper::map($usuarios, 'id', 'username');?>
+	<?php $result = ArrayHelper::map($usuarios, 'id', 'username');?>
+	<td>
+	<br><strong><div style="float: left; width: 300px; line-height:0.5em;">Usuario receptor</div></strong>
     <?php echo $form->field($model, 'ID_USER_RECEPTOR')->widget(Select2::className(), [
 	'data' => $result,
 	"options" => ['multiple' => true,
 		'placeholder' => 'Seleccione un usuario...',
 	],
-]);
-?>
+])->label('');
+?> <strong><div style="float: left; width: 300px; line-height:0.8em;">Notificacion</div></strong></td>
    <?=$form->field($model, 'NOTIFICACION')->widget(CKEditor::className(), [
 	'options' => ['rows' => 6],
 	'preset' => 'advanced',
-])?>
+])->label('');?>
     <div class="form-group">
         <div class="float-right">
             <?=Html::submitButton('Enviar', ['class' => 'btn btn-success'])?>
