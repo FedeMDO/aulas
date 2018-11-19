a11bbf4d5c58ba2214d86ceeee4f1caa72ec96c2
magikerN <braianpezet@gmail.com>
2018-11-19 17:19:44 -0300

    Panel muestra informacion real

--- basic/views/Admin/noti.php
+++ basic/views/Admin/noti.php
@@ -124,22 +124,3 @@ document.getElementById("defaultOpen").click();
 
 </body>
 </div>
-
-
-<!-- <nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Notificaciones</a>
-    </div>
-    <a href="../notificacion/create" class="btn btn-success">Enviar notificacion</a>
-  <a href="../notificacion/delete" class="btn btn-primary">Eliminar</a>
-  <a href="../notificacion/update" class="btn btn-primary">Modificar</a>
-  <a href="../notificacion/index" class="btn btn-primary">Ver Notificaciones</a>
-
-
-
-    <button class="btn btn-danger navbar-btn">Configuracion</button>
-  </div>
-</nav>
-
-<h1>HOLA ESTOY EN NOTIFICACIONES </h1> -->
--- basic/views/Admin/panel.php
+++ basic/views/Admin/panel.php
@@ -1,32 +1,43 @@
 <?php
 
 use yii\helpers\Html;
+use app\models\Users;
+use app\models\Carrera;
+use app\models\Materia;
+use app\models\Instituto;
+
 
 $this->title = 'Panel de administracion';
 ?>
+<?php
 
+$usuarios = Users::find()->count();
+$institutos = Instituto::find()->count();
+$carreras = Carrera::find()->count();
+$materias = Materia::find()->count();
 
+?>
 <div class="col-md-offset-1 col-md-3">
     <div class="well dash-box">
-    <h2><span class="glyphicon glyphicon-user" aria-hidden="true"></span> 203</h2>
+    <h2><span class="glyphicon glyphicon-user" aria-hidden="true"></span> <?=Html::encode("{$usuarios} ")?></h2>
     <h4>Usuarios registrados</h4>
     </div>
     </div>
     <div class="col-md-3">
     <div class="well dash-box">
-    <h2><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> 12</h2>
+    <h2><span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span> <?=Html::encode("{$carreras} ")?></h2>
     <h4>Carreras</h4>
     </div>
     </div>
     <div class="col-md-3">
     <div class="well dash-box">
-    <h2><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> 33</h2>
+    <h2><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> <?=Html::encode("{$materias} ")?></h2>
     <h4>Materias</h4>
     </div>
     </div>
     <div class="col-md-offset-1 col-md-3">
     <div class="well dash-box">
-    <h2><span class="glyphicon glyphicon-home" aria-hidden="true"></span> 10</h2>
+    <h2><span class="glyphicon glyphicon-home" aria-hidden="true"></span> <?=Html::encode("{$institutos} ")?></h2>
     <h4>Institutos</h4>
     </div>
     </div>
