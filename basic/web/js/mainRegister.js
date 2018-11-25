87a394cf1d6ff41dc20d5b3fe4adf8b4408c58e1
FedeMDO <federicomdo97@gmail.com>
2018-11-25 00:56:01 -0300

    minor fix

--- basic/web/js/mainRegister.js
+++ basic/web/js/mainRegister.js
@@ -3,6 +3,8 @@
     $("#checkNoInstituto").click(function() {
       if ($(this).is(":checked")) {
          $("#formregister-idinstituto").prop("disabled", true);
+         $("#formregister-idinstituto").val('');
+         //Seleccione un instituto...
       } else {
          $("#formregister-idinstituto").prop("disabled", false);  
       }
