d4fcfc1f037e7d7f381bd1b8b0d679add92d8cd8
Juan <juanignacio.ing7@gmail.com>
2018-11-27 17:52:23 -0300

    Ajustes visuales en Manual

--- basic/views/site/manual.php
+++ basic/views/site/manual.php
@@ -27,11 +27,11 @@ $this->title = 'Manual de usuario';
         <h3><b>¿Como ingresar a la aplicacion? </b></h3>
         <p>Para acceder a la aplicación, el usuario debe de hacer uso de sus credenciales de acceso (usuario o Email y contraseña).</p>
         <p>El acceso se otorgara dirigiendose al el siguiente enlace: http://yii.local/site/login</p>
-        <img width="900px" height="400" src="../image/manual/pastedmage0.png" />
+        <img width="765px" height="392" src="../image/manual/pastedmage0.png" />
         <br>
         <br>
         <p>Una vez ingresado su usuario y contraseña, pulse el botón ingresar para ingresar al sistema. Si todo sale correctamente, aparecerá en el inicio: </p>
-        <img width="900px" height="400" src="../image/manual/inicio.png" />
+        <img width="819px" height="417" src="../image/manual/inicio.png" />
         <br>
         <br>
         <h3><b>Recuperar contraseña:</b></h3> 
@@ -69,7 +69,7 @@ $this->title = 'Manual de usuario';
                     <h3><b>¿Como acceder a sedes?</b></h3>
                     <p>Para acceder a la aplicación se utilizará el siguiente enlace:</p>
                     <p>http://yii.local/sede/vistav </p>
-                    <img width="1000px" height="300" src="../image/manual/vistasedes.png" />
+                    <img width="673px" height="270" src="../image/manual/vistasedes.png" />
                     <br>
                     <br>
                     <p>En esta vista, nos aparecerá las sedes de la universidad.</p>
@@ -91,13 +91,13 @@ $this->title = 'Manual de usuario';
               <h3><b>Crear sedes</b></h3>
                 <br>
                 <p>Para la creación de sedes presionaremos el botón de la esquina superior izquierda:<p>
-                <img width="911px" height="490" src="../image/manual/createsedes.png" />
+                <img width="638px" height="343" src="../image/manual/createsedes.png" />
                 <br>
                 <p>Se desplegará el siguiente menú de opciones y seleccionaremos <b>“Creación de sedes”:</b></p>
                 <img width="200px" height="378" src="../image/manual/menu.png" />
                 <br><br>
                 <p>Nos saldra el siguiente formulario:</b>:</p>
-                <img width="500px" height="500" src="../image/manual/crearsede.png" />
+                <img width="504px" height="538" src="../image/manual/crearsede.png" />
                 <br><br>
                 <ul>
                     <p><b>Campos a rellenar:</b><p>
@@ -127,13 +127,13 @@ $this->title = 'Manual de usuario';
               <h3><b>Crear comisiones</b></h3>
                 <br>
                 <p>Para la creación de comisiones presionaremos el botón de la esquina superior izquierda:<p>
-                <img width="911px" height="490" src="../image/manual/createsedes.png" />
+                <img width="638px" height="343" src="../image/manual/createsedes.png" />
                 <br>
                 <p>Se desplegará el siguiente menú de opciones y seleccionaremos <b>“Creación de comisiones”:</b></p>
                 <img width="200px" height="378" src="../image/manual/menu.png" />
                 <br><br>
                 <p>Nos saldra el siguiente formulario:</b></p>
-                <img width="500px" height="500" src="../image/manual/crearcomi.png" />
+                <img width="296px" height="348" src="../image/manual/crearcomi.png" />
                 <br><br>
                 <p>Campos a rellenar:<p>
                 <p><b>Número:</b>&nbsp; Debe poner el numero de comisión.</p>
@@ -157,7 +157,7 @@ $this->title = 'Manual de usuario';
                 <h3><b>Creación de materias</b></h3>
                 <br>
                 <p>Para la creación de materias presionaremos el botón de la esquina superior izquierda:<p>
-                <img width="911px" height="490" src="../image/manual/createsedes.png" />
+                <img width="638px" height="343" src="../image/manual/createsedes.png" />
                 <br>
                 <p>Se desplegará el siguiente menú de opciones y seleccionaremos <b>“Crear materia”:</b></p>
                 <img width="200px" height="378" src="../image/manual/menu.png" />
@@ -188,7 +188,7 @@ $this->title = 'Manual de usuario';
               <h3><b>Filitrar aulas</b></h3>
                 <br>
                 <p>Para ingresar al filtro de aulas presionaremos el botón de la esquina superior izquierda:<p>
-                <img width="911px" height="490" src="../image/manual/createsedes.png" />
+                <img width="638px" height="343" src="../image/manual/createsedes.png" />
                 <br>
                 <p>Se desplegará el siguiente menú de opciones y seleccionaremos <b>“Filtrar aulas”:</b></p>
                 <img width="200px" height="378" src="../image/manual/menu.png" />
@@ -277,12 +277,12 @@ $this->title = 'Manual de usuario';
                 <br>
                 <p>Deberá ir a sedes, como lo indica la flecha azul:</p>
                 <br>
-                <img width="1097px" height="682" src="../image/manual/edin.png" />
+                <img width="803px" height="410" src="../image/manual/edin.png" />
                 <br>
 
                 <p><b></b>&nbsp; Una vez entrada a las sedes estas aparecerán al presionar el boton entrar con el cual se podrá ingresar y ver la lista de edificios. </p>
                 <br>
-                <img width="1097px" height="523" src="../image/manual/edificios.png" />
+                <img width="659px" height="314" src="../image/manual/edificios.png" />
                 <br>
                 <br>
               </div>
@@ -298,7 +298,7 @@ $this->title = 'Manual de usuario';
               <div class="panel-body">
                 <br>
                 <p>Para la creación de edificios usted deberá ir donde indica la flecha:<p>
-                <img width="1048x" height="590" src="../image/manual/creacionedi02.png" />
+                <img width="682x" height="384" src="../image/manual/creacionedi02.png" />
                 <br>
                 <p>Una vez seleccionado el botón, se desplegara el siguiente menu donde seleccionaremos "<b>Crear edificio</b>"</p>
                 <img width="199px" height="243" src="../image/manual/menuedi.png" />
@@ -355,7 +355,7 @@ $this->title = 'Manual de usuario';
                 <br>
                 <br>
                 <p>Una vez dentro de la vista de sedes debera presionar el botón entrar:</p>
-                <img width="1097px" height="586" src="../image/manual/aula02.png" />
+                <img width="672px" height="293" src="../image/manual/aula02.png" />
                
 
               </div>
@@ -373,7 +373,7 @@ $this->title = 'Manual de usuario';
               <h3><b>¿Como crear "Aulas"?</b></h3>
               <br>
               <p>Para la creación de Aulas usted deberá ir donde indica la flecha:</p>
-              <img width="1097px" height="611" src="../image/manual/aulas.png" />
+              <img width="760px" height="367" src="../image/manual/aulas.png" />
               <br><br>
                           <p>Se desplegará el siguiente menú de opciones y seleccionaremos “Crear aulas”<p>
                           <img width="176px" height="336" src="../image/manual/menuCrearAula.png" />
@@ -425,7 +425,7 @@ $this->title = 'Manual de usuario';
       <p>Para acceder a institutos, se debe ingresar previamente al sistema.</p>
       <b><p>Ingresar a institutos</b><p>
       <p>Deberá ir a institutos, como lo indica la flecha negra:</p>
-      <img width="1018px" height="444" src="../image/manual/institutos.png" />
+      <img width="713px" height="311" src="../image/manual/institutos.png" />
       <br><br>
       <p>En la siguiente imagen se podra apreciar los usuarios pertenecientes a cada instituto que seleccionemos:</p>
       <img width="419px" height="496" src="../image/manual/instiExpand.png" />
@@ -448,7 +448,7 @@ $this->title = 'Manual de usuario';
       <p>Para acceder a Carreras se debe ingresar previamente al sistema.</p>
       <b><p>Ingresar a Carreras</b><p>
       <p>Seleccionaremos donde donde indica la flecha para ingresar a carreras:</p>
-      <img width="1100px" height="539" src="../image/manual/carrerasind.png" />
+      <img width="660px" height="324" src="../image/manual/carrerasind.png" />
       <br><br>
       <p>Como se aprecia en la imagen, aparecerá las materias pertenecientes a cada carrera que seleccionemos:</p>
       <img width="434px" height="571" src="../image/manual/infcarreras.png" />
@@ -481,7 +481,7 @@ $this->title = 'Manual de usuario';
               <div class="panel-body">
               <h3><b>¿Como acceder a "Administración"? </b></h3>
               <p>Debe ir donde indica la flecha:</p>
-              <img width="1097px" height="146" src="../image/manual/adm.png" />
+              <img width="803px" height="43" src="../image/manual/adm.png" />
               <br><br>
               <p>Nos aparecera tres modulos en administración: Registro de usuario, Panel de administración y Gestionar Usuarios:</p>
               <img width="243px" height="160" src="../image/manual/menuAdmin.png" />
@@ -501,7 +501,7 @@ $this->title = 'Manual de usuario';
                       <p>Para acceder al registro de usuarios, se debe ser Admin, ya que es el único capaz de generar usuarios para el sistema. 
                       Accederemos al registro a través del siguiente el siguiente enlace:
                       http://yii.local/site/register </p>
-                      <img width="514px" height="500" src="../image/manual/reg.png" />
+                      <img width="514px" height="612" src="../image/manual/reg.png" />
                       <br>
                       <br>
                       <ul>
@@ -540,57 +540,57 @@ $this->title = 'Manual de usuario';
                        <p>En panel de administración usted podrá ver en el inicio algunas estadísticas generales como: el número de usuarios
                         registrados, de carreras, de materias y institutos. Además en la columna izquierda podrá entrar a los paneles de: 
                         aulas, carreras, comisiones, edificios, institutos, materias, notificaciones, recursos y de sedes..</p>
-                        <img width="971px" height="385" src="../image/manual/estadistAdmin.png" />
+                        <img width="651px" height="258" src="../image/manual/estadistAdmin.png" />
                         <br><br>
                         <h3><b>Paneles</b></h3>
                         <br>
                         <p><b>Panel de aula:</p></b>
-                        <img width="1097px" height="607" src="../image/manual/panelDeAula.png" />
+                        <img width="797px" height="365" src="../image/manual/panelDeAula.png" />
                         <br><br>
                         <p>En el panel de aulas veremos la lista de todas las aulas que se crearon. Adicionalmente, dispondremos de la opción de “Crear aula”.</p>
                         <br>
                         <p><b>Panel de carrera:</p></b>
-                        <img width="1157px" height="510" src="../image/manual/panelCarrera.png" />
+                        <img width="695px" height="306" src="../image/manual/panelCarrera.png" />
                         <br><br>
                         <p>En el panel de carreras veremos la lista de todas las carreras que se crearon. Adicionalmente, dispondremos de la opción de “Crear carrera”.</p>
                         <br>
                         <p><b>Panel de comisiones:</p></b>
-                        <img width="1090px" height="513" src="../image/manual/panelComisiones.png" />
+                        <img width="654px" height="308" src="../image/manual/panelComisiones.png" />
                         <br><br>
                         <p>En el panel de comisiones veremos la lista de todas las comisiones existentes. Además, dispondremos de la opción de “Crear comision”.</p>
                         <br>
                         <p><b>Panel de edificios:</p></b>
-                        <img width="1030px" height="455" src="../image/manual/panelEdificios.png" />
+                        <img width="608px" height="273" src="../image/manual/panelEdificios.png" />
                         <br><br>
                         <p>En el panel de edificios veremos la lista de todas los edificios que se crearon. Adicionalmente, dispondremos de la opción de “Crear edificio”.</p>
                         <br>
 
                         <p><b>Panel de institutos:</p></b>
-                        <img width="1051px" height="377" src="../image/manual/panelInstitutos.png" />
+                        <img width="631px" height="227" src="../image/manual/panelInstitutos.png" />
                         <br><br>
                         <p>En el panel de institutos veremos la lista de todas los institutos que se crearon. Adicionalmente, dispondremos de la opción de “Crear instituto”.</p>
                         <br>
 
                         <p><b>Panel de materias:</p></b>
-                        <img width="1116px" height="582" src="../image/manual/panelMaterias.png" />
+                        <img width="625px" height="326" src="../image/manual/panelMaterias.png" />
                         <br><br>
                         <p>En el panel de materias veremos la lista de todas los materias que se crearon. Adicionalmente, dispondremos de la opción de “Crear materia”.</p>
                         <br>
 
                         <p><b>Panel de notificaciones:</p></b>
-                        <img width="1140px" height="232" src="../image/manual/panelNoti.png" />
+                        <img width="684px" height="140" src="../image/manual/panelNoti.png" />
                         <br><br>
                         <p>En el panel de notificaciones veremos la lista de notificaciones que enviamos o recibimos.</p>
                         <br>
 
                         <p><b>Panel de recursos:</p></b>
-                        <img width="1118px" height="249" src="../image/manual/panelRecursos.png" />
+                        <img width="671px" height="150" src="../image/manual/panelRecursos.png" />
                         <br><br>
                         <p>En el panel de recursos veremos la lista de recursos que se crearon. Adicionalmente, dispondremos de la opción de “Crear recurso”.</p>
                         <br>
 
                         <p><b>Panel de sedes:</p></b>
-                        <img width="1113px" height="476" src="../image/manual/panelSedes.png" />
+                        <img width="668px" height="286" src="../image/manual/panelSedes.png" />
                         <br><br>
                         <p>En el panel de sedes veremos la lista de sedes que se crearon. Adicionalmente, dispondremos de la opción de “Crear sede”.</p>
 
@@ -611,7 +611,7 @@ $this->title = 'Manual de usuario';
                     
                     <p>Si entramos a gestión de usuarios nos aparecera la siguiente ventana:</p>
 
-                    <img width="1097px" height="376" src="../image/manual/gestionDeUsuarios.png" />
+                    <img width="815px" height="256" src="../image/manual/gestionDeUsuarios.png" />
                     <br>
                     <br>
                     <p> Si presionamos el botón "editar" de cualquier usuario, nos aparecera el siguiente formulario:</p>
@@ -671,10 +671,10 @@ $this->title = 'Manual de usuario';
                 <h3><b><p>Ingresar a Notificaciones</b><p></h3>
                 <br>
                 <p>Seleccionaremos donde donde indica la flecha para ingresar a Notificaciones:</p>
-                <img width="983px" height="168" src="../image/manual/Noti.png" />
+                <img width="590px" height="101" src="../image/manual/Noti.png" />
                 <br><br>
                 <p>En notificaciones podremos enviar mensajes a otros usuarios del sistema. Además, se dispondrá de un buzón de notificaciones recibidas y un buzón de notificaciones enviadas.<p>
-                <img width="1167px" height="173" src="../image/manual/NotiMenu.png" />
+                <img width="759px" height="113" src="../image/manual/NotiMenu.png" />
 
 
 
@@ -691,7 +691,7 @@ $this->title = 'Manual de usuario';
               <div class="panel-body">
 
               <p>Para enviar una notificación nos dirigiremos a donde indica la flecha de la imagen:<p>
-              <img width="999px" height="624" src="../image/manual/EnvioNoti.png" />
+              <img width="580px" height="362" src="../image/manual/EnvioNoti.png" />
               <br>
               <ul>
               <b><p>Campos:</p></b>
@@ -798,11 +798,11 @@ $this->title = 'Manual de usuario';
 
                             <p>Debemos dirigirnos donde indica la flecha:</p>
                             <br>
-                            <img width="1097px" height="577" src="../image/manual/newEvent.png" />
+                            <img width="769px" height="347" src="../image/manual/newEvent.png" />
                             <br><br>
                             <p>Una vez hecho esto, nos aparecerá el siguiente formulario:</p>
                             <br>
-                            <img width="867px" height="584" src="../image/manual/formEvent.png" />
+                            <img width="521px" height="351" src="../image/manual/formEvent.png" />
 
                             <br><br>
                             <b><p>Campos:</p></b>
@@ -823,7 +823,7 @@ $this->title = 'Manual de usuario';
                             <br>
                             <p>En la siguiente imagen se puede apreciar la vista paronamica del evento por día:
                             <br><br>
-                            <img width="1170px" height="522" src="../image/manual/vistaporDia.png" />
+                            <img width="702px" height="314" src="../image/manual/vistaporDia.png" />
                             <br><br>
 
                             <p><b><h4>Vista de materia por Semana</p></b></h4>
@@ -832,16 +832,16 @@ $this->title = 'Manual de usuario';
                             <br>
                             <p><b>Ejemplo:&nbsp; Semana 19 de noviembre al 24 de noviembre:</p></b> 
                             <br>
-                            <img width="1143px" height="328" src="../image/manual/vistaSemana01.png" />
+                            <img width="686px" height="197" src="../image/manual/vistaSemana01.png" />
                             <br><br>
                             <p><b>Ejemplo:&nbsp; Semana 26 de noviembre al 1 de diciembre:</p></b> 
                             <br>
-                            <img width="1094px" height="555" src="../image/manual/vistaSemana.png" />
+                            <img width="657px" height="333" src="../image/manual/vistaSemana.png" />
                             <br><br>
                             <h4><p><b>Vista de materia por Mes</p></b></h4>
                             <br>
                             <p>En la próxima imagen verá con más claridad cómo la materia que eligió está planificada en todo el mes, el mismo día.</p>
-                            <img width="1097px" height="598" src="../image/manual/vistaporMes.png" />
+                            <img width="747px" height="359" src="../image/manual/vistaporMes.png" />
                             <br><br>
 
                   </div>
