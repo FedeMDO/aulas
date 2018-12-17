cb518186871d3d558c0e0b537bc1388c7bd36006
magikerN <braianpezet@gmail.com>
2018-12-16 23:13:15 -0300

    minor fix

--- basic/views/site/noti.php
+++ basic/views/site/noti.php
@@ -11,7 +11,19 @@ use yii\helpers\Url;
 <header>
     <!-- jQuery CDN - Slim version (=without AJAX) -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-
+    <style>
+        @media (max-width: 768px) {
+            #sidebar {
+                margin-left: -250px;
+            }
+            #sidebar.active {
+                margin-left: 0;
+            }
+            #sidebarCollapse span {
+                display: none;
+            }
+        }
+    </style>
 </header>
 
 <div class="wrapper">
