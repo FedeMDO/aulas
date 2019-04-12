5626eebfe3bb79605706e65da3ddec7258a509f4
FedeMDO <fedee.piola@gmail.com>
2019-04-12 20:43:02 -0300

    base de datos aws

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=aulas_db',
+    'dsn' => 'mysql:host=aulasinstance.chu699oskawz.sa-east-1.rds.amazonaws.com;dbname=aulas_db',
     'username' => 'root',
-    'password' => '',
+    'password' => 'aulas2018+-',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
