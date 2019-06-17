73143e39a9df006495d4903f33fb81dabac94522
FedeMDO <federicomdo97@gmail.com>
2019-06-17 17:34:01 -0300

    validacion registro form jquery - usuario normal si o si necesita instituto

--- app/web/js/mainRegister.js
+++ app/web/js/mainRegister.js
@@ -9,4 +9,20 @@ $(document).ready(function () {
          $("#formregister-idinstituto").prop("disabled", false);
       }
    });
+   $("#formregister-rol").change(function (e) {
+      e.preventDefault();
+      if ($("#formregister-rol option:selected").val() == 1) {
+
+         $("#checkNoInstituto").prop("checked", false);
+         $("#checkNoInstituto").prop("disabled", true);
+
+         $('option[value=""]', $("#formregister-idinstituto")).remove();
+      }
+      else {
+         $($("#formregister-idinstituto")).prepend('<option value>Seleccione un instituto...</option>');
+         $($("#formregister-idinstituto")).val($('option:first', $("#formregister-idinstituto")).val());
+         $("#checkNoInstituto").prop("checked", false);
+         $("#checkNoInstituto").prop("disabled", false);
+      }
+   });
 });
