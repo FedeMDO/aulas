c90a2c276445ed95656764a9d1dc2862179a4bd0
ivanzapata <ivanzapata.f@gmail.com>
2019-06-21 13:50:34 -0300

    fix view sede edificios

--- app/views/edificio/edifilter.php
+++ app/views/edificio/edifilter.php
@@ -1,4 +1,3 @@
-
 <?php
 use yii\helpers\Html;
 use yii\helpers\Url;
@@ -11,88 +10,93 @@ $this->title = 'Edificios';
 ?>
 
 <header>
-    <!-- jQuery CDN - Slim version (=without AJAX) -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <!-- jQuery CDN - Slim version (=without AJAX) -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+  </script>
 
 </header>
 <?= \yii\helpers\Url::remember(); ?>
 <div class="wrapper">
-    <!-- Sidebar  -->
-    <nav id="sidebar" class="active">
-        <!-- header -->
-        <!--<div class="sidebar-header">
+  <!-- Sidebar  -->
+  <nav id="sidebar" class="active">
+    <!-- header -->
+    <!--<div class="sidebar-header">
         </div> -->
-        <ul class="list-unstyled components">
-            <li><a href="../aula/buscador">Filtrar aulas <span class="glyphicon glyphicon-search"></span></a></li>
-            <li><a href="../materia/buscador">Filtrar materias <span class="glyphicon glyphicon-search"></span></a></li>
-            <li><a href="../edificio/scheduler?id_sede=<?= Html::encode("{$idsede}") ?>" class="btn miBoton btn-md btn-vistav" role="button">Ver eventos por edificio</a></li>
-            <?php if (app\models\User::isUserAdmin(Yii::$app->user->identity->id)) : ?>
-            <li><a href="../edificio/create">Crear edificio <span class="glyphicon glyphicon-plus"></span></a></li>
-            <li><a href="../edificio/restrischeduler?id_sede=<?= Html::encode("{$idsede}") ?>" class="btn miBoton btn-md btn-vistav" role="button">Restricciones edificios</a></li>
-            <?php endif; ?>
-        </ul>
-        <!-- Parte de abajo de sidenav -->
-        <ul class="list-unstyled CTAs">
-            <li><a href="../sede/vistav" class="download">Volver a sedes</a></li>
-            </ul>
-    </nav>
-    <!--termina sidebar-->
-    
+    <ul class="list-unstyled components">
+      <li><a href="../aula/buscador">Filtrar aulas <span class="glyphicon glyphicon-search"></span></a></li>
+      <li><a href="../materia/buscador">Filtrar materias <span class="glyphicon glyphicon-search"></span></a></li>
+      <li><a href="../edificio/scheduler?id_sede=<?= Html::encode("{$idsede}") ?>" class="btn miBoton btn-md btn-vistav"
+          role="button">Ver eventos por edificio</a></li>
+      <?php if (app\models\User::isUserAdmin(Yii::$app->user->identity->id)) : ?>
+      <li><a href="../edificio/create">Crear edificio <span class="glyphicon glyphicon-plus"></span></a></li>
+      <li><a href="../edificio/restrischeduler?id_sede=<?= Html::encode("{$idsede}") ?>"
+          class="btn miBoton btn-md btn-vistav" role="button">Restricciones edificios</a></li>
+      <?php endif; ?>
+    </ul>
+    <!-- Parte de abajo de sidenav -->
+    <ul class="list-unstyled CTAs">
+      <li><a href="../sede/vistav" class="download">Volver a sedes</a></li>
+    </ul>
+  </nav>
+  <!--termina sidebar-->
 
-    <div id="content">
-        <!-- boton de sidebar-->
-        <div class="container-fluid">
-            <button type="button" id="sidebarCollapse" class="btn btn-primary"><i class="glyphicon glyphicon-align-justify"></i><span> Menu</span></button>
-        </div>
-        <!-- Contenido de la pagina -->
-        <h2 class=titulo><?php if (count($edificio) != 0) {
+
+  <div id="content">
+    <!-- boton de sidebar-->
+    <div class="container-fluid">
+      <button type="button" id="sidebarCollapse" class="btn btn-primary"><i
+          class="glyphicon glyphicon-align-justify"></i><span> Menu</span></button>
+    </div>
+    <!-- Contenido de la pagina -->
+    <h2 class=titulo><?php if (count($edificio) != 0) {
                     echo ("Edificios Disponibles en la sede ");
                     echo (Html::encode("{$edificio[0]->sEDE->NOMBRE}"));
                   } else {
                   } ?>
-  </h2>
+    </h2>
     <?php foreach ($edificio as $edificio) : ?>
     <div class="row3">
-        <div id="columna" class="col-sm-8 col-md-4 active">
-      <div class="box">
-          
-        <div class="imgBox">
-          <img src="../image/edi_<?= Html::encode("{$edificio->ID}") ?>.png">
-          
-        </div>
-
-        <div class="details">
-
-          <div class="content" style=>
-
-            <div class="thumbnail sede">
-              <div class="caption">
-                <h4 style="text-align:center;"><?= Html::encode("{$edificio->NOMBRE} ") ?></h4>
-                <img  src="../image/aulaicon.png" alt="  height="50" width="50">
-                <?= Html::encode("{$edificio->CANTIDAD_AULAS} ") ?>
-                <p></p>
-                <a href="../aula/aulafilter?id=<?= Html::encode("{$edificio->ID}") ?>" class="btn btn-info" role="button">Entrar</a>
-                <?php if (app\models\User::isUserAdmin(Yii::$app->user->identity->id)) : ?>
-                  <a href="../edificio/update?id=<?= Html::encode("{$edificio->ID}") ?>" class="btn btn-primary" role="button">Modificar</a>
-                <?php endif; ?>
+      <div id="columna" class="col-sm-8 col-md-4 active">
+        <div class="box">
+          <div class="imgBox">
+            <img src="../image/edi_<?= Html::encode("{$edificio->ID}") ?>.png">
+            <div class="details">
+            <div class="content">
+              <div class="thumbnail sede">
+                <div class="caption">
+                  <h4 style="text-align:center;"><?= Html::encode("{$edificio->NOMBRE} ") ?></h4>
+                  <img src="../image/aulaicon.png" alt="  height=" 50" width="50">
+                  <?= Html::encode("{$edificio->CANTIDAD_AULAS} ") ?>
+                  <p></p>
+                  <a href="../aula/aulafilter?id=<?= Html::encode("{$edificio->ID}") ?>" class="btn btn-info"
+                    role="button">Entrar</a>
+                  <?php if (app\models\User::isUserAdmin(Yii::$app->user->identity->id)) : ?>
+                  <a href="../edificio/update?id=<?= Html::encode("{$edificio->ID}") ?>" class="btn btn-primary"
+                    role="button">Modificar</a>
+                  <?php endif; ?>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        </div>
+          </div>
+          <h4 class="titulo"><?= Html::encode("{$edificio->NOMBRE} ") ?></h4>
+          <h4 class="titulo">Cantidad Aulas: <?= Html::encode("{$edificio->CANTIDAD_AULAS}")?></h4>
         </div>
       </div>
+    </div>
     <?php endforeach; ?>
 
-     </div> <!-- final del contenido-->
+  </div> <!-- final del contenido-->
 </div> <!-- final del wraper-->
 
 <!-- script para sidebar -->
 <script type="text/javascript">
-    $(document).ready(function () {
-        $('#sidebarCollapse').on('click', function () {
-            $('#sidebar').toggleClass('active');
-        });
+  $(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
     });
+  });
 </script>
+
 </html>
--- app/views/sede/vistav.php
+++ app/views/sede/vistav.php
@@ -1,4 +1,3 @@
-
 <?php
 use yii\helpers\Html;
 use yii\widgets\LinkPager;
@@ -8,68 +7,63 @@ $this->title = 'Sedes';
 ?>
 <?= \yii\helpers\Url::remember(); ?>
 <header>
-    <!-- jQuery CDN - Slim version (=without AJAX) -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-
-</header>
+<!-- jQuery CDN - Slim version (=without AJAX) -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+</script>
 
 <div class="wrapper">
-    <!-- Sidebar  -->
+    <!-- Sidebar -->
     <nav id="sidebar" class="active">
-        <!-- header -->
-        <!--<div class="sidebar-header">
-        </div> -->
         <ul class="list-unstyled components">
-            <li><a href="../aula/buscador">Filtrar aulas <span class="glyphicon glyphicon-search"></span></a></li>
-            <li><a href="../materia/buscador">Filtrar materias <span class="glyphicon glyphicon-search"></span></a></li>
-            <?php if (app\models\User::isUserAdmin(Yii::$app->user->identity->id)) : ?>
-            <li><a href="../sede/create">Crear sede <span class="glyphicon glyphicon-plus"></span></a></li>
-            <li><a href="../comision/create">Crear comision <span class="glyphicon glyphicon-plus"></span></a></li>
-            <li><a href="../materia/create" >Crear materias <span class="glyphicon glyphicon-plus"></span></a></li>
-            <?php endif ?>
+            <li><a href="../aula/buscador">Filtrar aulas <span class="glyphicon glyphicon-search"></a></li>
+            <li><a href="../materia/buscador">Filtrar materias <span class="glyphicon glyphicon-search"></a></li>
+            <?php if(app\models\User::isUserAdmin(Yii::$app->user->identity->id)) : ?>
+            <li><a href="../sede/create">Crear sede <span class="glyphicon glyphicon-plus"></a></li>
+            <li><a href="../comision/create">Crear comision <span class="glyphicon glyphicon-plus"></a></li>
+            <li><a href="../materia/create">Crear materias <span class="glyphicon glyphicon-plus"></a></li>
+            <?php endif; ?>
         </ul>
     </nav>
-    <!--termina sidebar-->
     <div id="content">
-        <!-- boton de sidebar-->
         <div class="container-fluid">
             <button type="button" id="sidebarCollapse" class="btn btn-primary"><i class="glyphicon glyphicon-align-justify"></i><span> Menu</span></button>
         </div>
-        <!-- Contenido de la pagina -->
-        <h2 class=titulo>Sedes Disponibles</h2>
+        <h2 class="titulo">Sedes Disponibles</h2>
         <?php foreach ($sede as $sede) : ?>
-            <div id="columna" class="col-sm-12 col-md-4 active"> <!-- grid -->
-              <div class="box"> 
-                <div class="imgBox">
-                  <img src="../image/sede_<?= Html::encode("{$sede->ID}") ?>.png">
-                </div>
-              <div class="details">
-                <div class="content">
-                  <div class="thumbnail sede">
-                    <div class="caption">
-                      <h4><?= Html::encode("{$sede->NOMBRE} ") ?> </h4>
-                      <p><?= Html::encode("{$sede->CALLEYNUM}") ?> -<?= Html::encode(" {$sede->LOCALIDAD} ") ?></p>
-                      <p></p>
-                      <a href="../edificio/edifilter?id=<?= Html::encode("{$sede->ID}") ?>" class="btn btn-info" role="button">Entrar</a>
-                      <?php if (app\models\User::isUserAdmin(Yii::$app->user->identity->id)) : ?>
-                        <a href="../sede/update?id=<?= Html::encode("{$sede->ID}") ?>"  class="btn btn-primary" role="button">Modificar</a>
-                      <?php endif; ?>
+            <div id="columna" class="col-sm-12 col-md-4 active">
+                <div class="box">
+                    <div class="imgBox">
+                        <img src="../image/sede_<?= Html::encode("{$sede->ID}") ?>.png" alt="">
+                        <div class="details">
+                            <div class="content">
+                                <div class="thumbnail sede">
+                                    <div class="caption">
+                                    <h4><?= Html::encode("{$sede->NOMBRE}")?></h4>
+                                    <a href="../edificio/edifilter?id=<?= Html::encode("{$sede->ID}") ?>" class="btn btn-info"
+                                     role="button">Entrar</a>
+                                    <?php if (app\models\User::isUserAdmin(Yii::$app->user->identity->id)) : ?>
+                                    <a href="../sede/update?id=<?= Html::encode("{$sede->ID}") ?>" class="btn btn-primary"
+                                     role="button">Modificar</a>
+                                    <?php endif; ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                  </div>
+                    <h4 class="titulo"><?= Html::encode("{$sede->NOMBRE}")?></h4>
+                    <h4 class="titulo"><?= Html::encode("{$sede->CALLEYNUM}")?>-<?= Html::encode(" {$sede->LOCALIDAD}")?></h4>
                 </div>
-              </div>
             </div>
-          </div>
         <?php endforeach; ?>
     </div>
-  </div> <!-- final del contenido-->
-</div> <!-- final del wraper-->
-<!-- script para sidebar -->
+</div>
+
 <script type="text/javascript">
-    $(document).ready(function () {
-        $('#sidebarCollapse').on('click', function () {
-            $('#sidebar').toggleClass('active');
-        });
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
     });
+});
 </script>
 </html>
